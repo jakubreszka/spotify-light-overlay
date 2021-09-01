b1e46119024db03558b6e2fdc82ac85a7464eac7
jakubreszka <jakubreszka97@gmail.com>
2021-09-01 22:54:59 +0200

    image load attemps

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import spotipy
 import sys
+import requests
 from PySide6 import QtCore, QtWidgets, QtGui
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
@@ -24,24 +25,34 @@ current_song = sp.currently_playing()
 artist_name = current_song['item']['artists'][0]['name']
 album_name = current_song['item']['album']['name']
 song_name = current_song['item']['name']
+album_cover = current_song['item']['album']['images'][2]['url']
 
 print(f"""CURRENTLY PLAYED SONG
 Artist: {artist_name}
 Album: {album_name}
-Song Title: {song_name}""")
+Song Title: {song_name}
+Album Cover: {album_cover}""")
 
-class MyWidget(QtWidgets.QWidget):
+class SpotifyApp(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.text = QtWidgets.QLabel(f"{artist_name}, {album_name}, {song_name}",
+        self.artist_text = QtWidgets.QLabel(f"Artist: {artist_name}",
                                      alignment=QtCore.Qt.AlignCenter)
+        self.song_text = QtWidgets.QLabel(f"Song: {song_name}",
+                                     alignment=QtCore.Qt.AlignCenter)
+        self.album_text = QtWidgets.QLabel(f"Song: {album_name}",
+                                     alignment=QtCore.Qt.AlignCenter)
+        pic = QtGui.QImage().loadFromData(requests.get(album_cover).content)
+        self.lbl = QtWidgets.QLabel(pic)
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.artist_text)
+        self.layout.addWidget(self.song_text)
+        self.layout.addWidget(self.album_text)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
+    widget = SpotifyApp()
     widget.resize(800, 600)
     widget.show()
 
